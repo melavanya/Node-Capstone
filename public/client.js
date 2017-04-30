25196859ea054cac24a7d61d2cb513a597a6850b
Lavanya S <lavanya.sethuraman23@gmail.com>
2017-04-30 16:09:13 -0400

    MVP trial

--- public/client.js
+++ public/client.js
@@ -1,55 +1,48 @@
-var TMDB_BASE_URL = "https://api.themoviedb.org/3/search/movie";
+
 
 $(function() {
   search();
 });
 
-function getDataFromApi(searchTerm ,callback) {
-  var queryString = {
-    api_key: 'ccceb486c69dc202428bef9e18797cf3',
-    language: 'en-US',
-    query: searchTerm
-  };
-  $.getJSON(TMDB_BASE_URL, queryString, callback);
-}
-//   $.ajax({
-//     url: '/movieDB',
-//     type: 'GET',
-//     dataType: 'json',
-//     data: {"title":searchTerm}
-//   })
-//   .done(function(data) {
-//     console.log("success");
-//     console.log(data);
-//   })
-//   .fail(function() {
-//     console.log("error");
-//   })
-//   .always(function() {
-//     console.log("complete");
-//   });
-// }
+function getMovie(searchTerm){
+$.ajax({
+  url: '/movies',
+  dataType: 'json',
+  data: {
+    query:searchTerm
+  }
+})
+.done(function(data) {
+  console.log("success");
+  displaySearchData(data);
+})
+.fail(function() {
+  console.log("error");
+})
+.always(function() {
+  console.log("complete");
+});
 
+}
 
 function search() {
   $(".js-search-form").submit(function(e) {
     e.preventDefault();
     var searchTerm = $(this).find(".js-search-input").val();
-  //  console.log(searchTerm);
-    getDataFromApi(searchTerm, displaySearchData);
+    getMovie(searchTerm);
   });
 }
 
-
-
-
 function displaySearchData(dataJson){
-  console.log(dataJson);
- console.log(" Title: " + dataJson.results[0].title);
- console.log(" Release Date: " + dataJson.results[0].release_date);
- console.log(" Overview: " + dataJson.results[0].overview);
- var html='<p><img src="https://image.tmdb.org/t/p/w500' +dataJson.results[0].poster_path +'" > </p><p>Title: ' + dataJson.results[0].title + '</p><p>Release Date: ' + dataJson.results[0].release_date + '</p><p>Overview: ' + dataJson.results[0].overview +
- '</p>';
+  var genreElement = dataJson.Genre.map(function(genre) {
+    return '<li>'+ genre.name +'</li>';
+  });
+ var html='<p><img src="https://image.tmdb.org/t/p/w500' + dataJson.Poster +
+ '" > </p><p>Title: '+ dataJson.Title + '</p><p>Release Date: '
+  + dataJson.Release_date + '</p><p>Rating:'+ dataJson.Rating +
+  '</p><p>Duration:'+ dataJson.Duration+' Minutes</p><p><ul>' +genreElement+
+  '</ul></p><p>Overview: '
+   + dataJson.Overview +'</p>';
  $('.js-search-results').removeClass('invisible');
  $('.js-search-results').html(html);
 
--- server.js
+++ server.js
@@ -1,20 +1,78 @@
 var express = require('express');
 var app = express();
-const movies = new (require('tmdbapi'))({
-    apiv3: 'ccceb486c69dc202428bef9e18797cf3'
-});
+var TMDB_URL1 = "https://api.themoviedb.org/3/search/movie";
+var TMDB_URL2 = "https://api.themoviedb.org/3/movie/";
+
 var bodyParser = require('body-parser');
+var result = {};
+var Client = require('node-rest-client').Client;
+var client = new Client();
+var searchTerm;
+var movieId="";
 
+function getMovieId (searchTerm){
+  var queryString = {
+    parameters : { api_key: 'ccceb486c69dc202428bef9e18797cf3',
+    query: searchTerm }
+  };
+  return new Promise(function(resolve , reject){
+  client.get(TMDB_URL1, queryString ,function( data , response){
+    if(response.statusCode=== 200){
+      if(data.results.length !== 0){
+        movieId=data.results[0].id;
+        console.log(movieId);
+        resolve();
+      } else {
+        reject();
+      }
+    }
 
-
-app.get("/movieDB", (req, res) => {
-  console.log(`${req.body}`);
-  var data=0;
-  res.send({server:data});
 });
+});
+}
+
+function getDataFromApi(movieId) {
+  var queryString = {
+    parameters : { api_key: 'ccceb486c69dc202428bef9e18797cf3'
+    }
+  };
+  return new Promise(function(resolve , reject){
+    client.get(TMDB_URL2+movieId, queryString ,function( data , response){
+      if(response.statusCode=== 200){
+          result.Title = data.title;
+          result.Release_Date = data.release_date;
+          result.Overview = data.overview;
+          result.Poster = data.poster_path;
+          result.Genre = data.genres;
+          result.Rating = data.vote_average;
+          result.Duration = data.runtime;
+          console.log(result);
+          resolve(result);
+        }else {
+          reject();
+        }
+    });
+
+  });
+}
+
 
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080, () => console.log(
-  `Your app is listening on port ${process.env.PORT || 8080}`));
+app.get("/movies",(req,res) => {
+  getMovieId(req.query.query)
+  .then(function(){
+    getDataFromApi(movieId)
+      .then(function(){
+        res.json(result)
+      });
+
+
+  });
+});
+
+
+
+  app.listen(process.env.PORT || 8080, () => console.log(
+    `Your app is listening on port ${process.env.PORT || 8080}`));
